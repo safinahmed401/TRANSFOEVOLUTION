2e9cfed59687c3098152ae8b90461a1650ce59b7
safin <safinahmed5934@gmail.com>
2021-06-24 02:27:18 -0400

    SAFIN First push

---
+++ TransfoEvolution/src/test/java/step_defination/BookAnAppoinmentLayoutTest.java
@@ -0,0 +1,42 @@
+package step_defination;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class BookAnAppoinmentLayoutTest {
+
+	public static final String url ="https://transfotechacademy.com/";
+	public static WebDriver driver;
+	
+	@Before
+	public void loadBrowser() {
+	System.setProperty("webdriver.chrome.driver", "C:\\Users\\Safin\\Desktop\\Drivers"
+    		+ "\\ChromeDriver\\chromedriver.exe");
+	driver = new ChromeDriver();
+	driver.manage().window().maximize();
+	driver.manage().timeouts().pageLoadTimeout(20, TimeUnit.SECONDS);
+	driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+	driver.get(url);
+	}
+	@After
+	public void closeBrowser() {
+	driver.close();
+	}
+	
+	@Given("user is in home page")
+	public void user_is_in_home_page() {
+	   Assert.assertEquals(driver.getCurrentUrl(), url);
+	}
+}
+
+	
